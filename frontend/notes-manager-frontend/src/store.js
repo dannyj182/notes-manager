4f53a4c7e898cd34bc7f2c27951a8d48369243f8
Danny Alexander Jimenez Casares <dannyj182@gmail.com>
2024-01-09 02:55:51 -0300

    Front-end update in the backend project

--- frontend/notes-manager-frontend/src/store.js
+++ frontend/notes-manager-frontend/src/store.js
@@ -13,7 +13,7 @@ export default new Vuex.Store({
         token: '',
         username: '',
         responseLogin: '',
-        url: 'http://localhost:8080/',
+        url: '/',
     },
     actions : {
         desloguearse({commit}){
