7fc169cb41e5fb63e8203310bbba3e6062e7558c
Danny Alexander Jimenez Casares <dannyj182@gmail.com>
2024-05-05 13:30:34 -0300

    Enable JpaAuditing

--- src/main/java/com/dannyj182/notesmanager/NotesManagerApplication.java
+++ src/main/java/com/dannyj182/notesmanager/NotesManagerApplication.java
@@ -2,8 +2,10 @@ package com.dannyj182.notesmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class NotesManagerApplication {
 
 	public static void main(String[] args) {
