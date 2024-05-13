3f8a497b748e7050b8baf65a93a83cff6762d700
Danny Alexander Jimenez Casares <dannyj182@gmail.com>
2024-05-13 01:12:36 -0300

    Changes in the order of INoteService methods

--- src/main/java/com/dannyj182/notesmanager/service/INoteService.java
+++ src/main/java/com/dannyj182/notesmanager/service/INoteService.java
@@ -4,8 +4,8 @@ import com.dannyj182.notesmanager.model.dto.NoteDTO;
 import com.dannyj182.notesmanager.model.dto.ResponseDTO;
 
 public interface INoteService {
-    ResponseDTO saveNote(NoteDTO noteDTO);
     ResponseDTO findNotesByUser(Integer pageNumber, Integer pageSize, String[] sortBy, String sortDirection);
-    boolean deleteById(long noteId);
+    ResponseDTO saveNote(NoteDTO noteDTO);
     NoteDTO editNote(long noteId, NoteDTO noteDTO);
+    boolean deleteById(long noteId);
 }
