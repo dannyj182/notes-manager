dd3b0e49de9526ab1230f60026db0aff5ccc8af4
Danny Alexander Jimenez Casares <dannyj182@gmail.com>
2024-05-15 23:15:49 -0300

    Added createdBy and modifiedBy columns in Auditable classes

---
+++ src/main/java/com/dannyj182/notesmanager/audit/AuditUsername.java
@@ -0,0 +1,28 @@
+package com.dannyj182.notesmanager.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.lang.NonNull;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuditUsername implements AuditorAware<String> {
+
+    @Override
+    @NonNull
+    public Optional<String> getCurrentAuditor() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return Optional.empty();
+        }
+
+        String username = authentication.getPrincipal().toString();
+
+        return Optional.of(username);
+    }
+}
--- src/main/java/com/dannyj182/notesmanager/audit/Auditable.java
+++ src/main/java/com/dannyj182/notesmanager/audit/Auditable.java
@@ -1,11 +1,13 @@
-package com.dannyj182.notesmanager.model.entity;
+package com.dannyj182.notesmanager.audit;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
@@ -23,4 +25,14 @@ public class Auditable {
     @Column(columnDefinition = "DATETIME")
     @LastModifiedDate
     private LocalDateTime modifiedDate;
+
+    @Column
+    @CreatedBy
+    @JsonIgnore
+    private String createdBy;
+
+    @Column
+    @LastModifiedBy
+    @JsonIgnore
+    private String modifiedBy;
 }
